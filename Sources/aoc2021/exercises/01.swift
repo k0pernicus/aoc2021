359fefeb2463c3758809dc1602dcebfbb349c60f
k0pernicus <dev@0xc0ff33.me>
2021-12-01 09:21:27 +0100

    First exercise

--- Sources/aoc2021/exercises/01.swift
+++ Sources/aoc2021/exercises/01.swift
@@ -10,6 +10,11 @@ import XCTest
 
 class Ex01: Exercise {
     var name: String = "01"
+   
+    typealias InputPart1 = [Int]
+    typealias InputPart2 = [Int]
+    typealias OutputPart1 = Int
+    typealias OutputPart2 = Int
     
     private init() {
         let result = Exercises.shared.register(self.name)
@@ -22,9 +27,8 @@ class Ex01: Exercise {
         do {
             let input: Result<[Int?]> = try getInput(from: from, encodeFrom: toInt)
             switch input {
-            case .ok(_):
-                // TODO: Process
-                return .ok(0)
+            case .ok(let anyDepths):
+                return self.part1(value: anyDepths.compactMap{ $0 })
             case .error(let err):
                 return .error(err)
             }
@@ -38,9 +42,8 @@ class Ex01: Exercise {
         do {
             let input: Result<[Int?]> = try getInput(from: from, encodeFrom: toInt)
             switch input {
-            case .ok(_):
-                // TODO: Process
-                return .ok(0)
+            case .ok(let anyDepths):
+                return self.part2(value: anyDepths.compactMap{ $0 })
             case .error(let err):
                 return .error(err)
             }
@@ -50,12 +53,40 @@ class Ex01: Exercise {
         }
     }
     
-    internal func part1(value: Int) -> Result<Int> {
-        return .ok(0)
+    internal func part1(value depths: [Int]) -> Result<Int> {
+        if depths.count == 0 {
+            return .ok(0)
+        }
+        var previousMeasure: Int = depths[0]
+        var count = 0
+        for measure in depths[1...] {
+            if measure > previousMeasure {
+                count += 1
+            }
+            previousMeasure = measure
+        }
+        return .ok(count)
     }
     
-    internal func part2(value: Int) -> Result<Int> {
-        return .ok(0)
+    internal func part2(value depths: [Int]) -> Result<Int> {
+        let slidingWindowLength = 3
+        if depths.count < slidingWindowLength {
+            return .ok(0)
+        }
+        var slidingWindow: [Int] = [depths[0], depths[1], depths[2]]
+        var previousMeasure = slidingWindow.reduce(0, +)
+        var count = 0
+        var slidingIndex = 0
+        for measure in depths[slidingWindowLength...] {
+            slidingWindow[slidingIndex % slidingWindowLength] = measure
+            slidingIndex += 1
+            let newMeasure = slidingWindow.reduce(0, +)
+            if newMeasure > previousMeasure {
+                count += 1
+            }
+            previousMeasure = newMeasure
+        }
+        return .ok(count)
     }
     
     public static let shared = Ex01()
--- Tests/aoc2021Tests/Ex01Tests.swift
+++ Tests/aoc2021Tests/Ex01Tests.swift
@@ -9,12 +9,39 @@ import XCTest
 @testable import aoc2021
 
 final class Ex01Tests: XCTestCase {
+    let depthsTest: [Int] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    
     func testPart1() throws {
         // Some of the APIs that we use below are available in macOS 10.13 and above.
         guard #available(macOS 10.13, *) else {
             return
         }
-
-        XCTAssertEqual(true, true)
+        
+        let toFind: Int = 7
+        
+        switch Ex01.shared.part1(value: depthsTest) {
+        case .ok(let count):
+            XCTAssertEqual(count, toFind)
+        case .error(let err):
+            print("Error for part1: \(err)")
+            XCTFail()
+        }
+    }
+    
+    func testPart2() throws {
+        // Some of the APIs that we use below are available in macOS 10.13 and above.
+        guard #available(macOS 10.13, *) else {
+            return
+        }
+        
+        let toFind: Int = 5
+        
+        switch Ex01.shared.part2(value: depthsTest) {
+        case .ok(let count):
+            XCTAssertEqual(count, toFind)
+        case .error(let err):
+            print("Error for part1: \(err)")
+            XCTFail()
+        }
     }
 }
