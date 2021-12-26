fe5127140182b400901c55af3cc481689a6a6f52
k0pernicus <dev@0xc0ff33.me>
2021-12-26 02:37:49 +0100

    Submitted part2 of ex21

--- Sources/aoc2021/exercises/21.swift
+++ Sources/aoc2021/exercises/21.swift
@@ -82,17 +82,79 @@ class Ex21: Exercise {
             let sumDie: Int = (die..<die+3).map { $0 }.reduce(0, +)
             let newBoardSpace = board[(spaces[cPlayer] + sumDie - 1) % board.count]
             scores[cPlayer] += newBoardSpace; spaces[cPlayer] = newBoardSpace
-            print("Player \(cPlayer + 1) rolls \((die..<die+3).map { String($0) }.joined(separator: "+")) and moves to space \(spaces[cPlayer]) for a total score of \(scores[cPlayer]).")
             die += 3; nbDie += 3
             if scores[cPlayer] >= 1000 { break }
             cPlayer = cPlayer == 1 ? 0 : 1
         }
-        print("nb die: \(nbDie), player \(cPlayer) won!")
         return .ok(scores[cPlayer == 1 ? 0 : 1] * nbDie)
     }
     
+    struct State: Equatable, Hashable {
+        var p1Position: Int
+        var p1Score: Int
+        var p2Position: Int
+        var p2Score: Int
+        var p1ToPlay: Bool
+        
+        init(p1Position: Int, p2Position: Int) {
+            self.p1Position = p1Position
+            self.p2Position = p2Position
+            self.p1Score = 0
+            self.p2Score = 0
+            self.p1ToPlay = true
+        }
+    }
+    
     internal func part2(value data: [String]) -> Result<Int> {
-        return .ok(0)
+        // Tip for the dice roll outcomes:
+        // 3 4 4 5 5 5 6 6 7
+        // 4 5 5 6 6 6 7 7 8
+        // 5 6 6 7 7 7 8 8 9
+        let diceOutcomes: [(Int, Int)] = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
+        
+        guard let positions = findPositions(rawStrings: data) else {
+            return .error("cannot parse and find players positions")
+        }
+        
+        func countWins(state: State, cache: inout [State: (Int, Int)]) -> (Int, Int) {
+            if state.p1Score >= 21 {
+                return (1, 0)
+            }
+            if state.p2Score >= 21 {
+                return (0, 1)
+            }
+            var total = (0, 0)
+            for (steps, frequency) in diceOutcomes {
+                var nextState: State = state
+                if state.p1ToPlay {
+                    nextState.p1Position = ((state.p1Position + steps - 1) % 10) + 1
+                    nextState.p1Score += nextState.p1Position
+                    nextState.p1ToPlay = false
+                } else {
+                    nextState.p2Position = ((state.p2Position + steps - 1) % 10) + 1
+                    nextState.p2Score += nextState.p2Position
+                    nextState.p1ToPlay = true
+                }
+                
+                var cacheState = cache[nextState]
+                if cacheState == nil {
+                    let res = countWins(state: nextState, cache: &cache);
+                    cache[nextState] = res;
+                    cacheState = res
+                }
+                
+                total.0 += (cacheState!.0 * frequency)
+                total.1 += (cacheState!.1 * frequency)
+            }
+            
+            return total
+        }
+        
+        var cache: [State: (Int, Int)] = [:]
+        
+        let wins = countWins(state: State(p1Position: positions[0], p2Position: positions[1]), cache: &cache)
+        
+        return .ok(max(wins.0, wins.1))
     }
     
     public static let shared = Ex21()
